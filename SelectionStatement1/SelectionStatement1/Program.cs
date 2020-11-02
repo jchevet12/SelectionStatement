4c8f6947bd8c4f06526d4c42b127c3d0968a07cf
justin <jcawthon12@yahoo.com>
2020-11-02 17:13:20 -0500

    else exercise

---
+++ SelectionStatement1/SelectionStatement1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionStatement1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FavNumber = 3;
+            {
+                Console.WriteLine("guess the favorite number, go low");
+                var userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < FavNumber)
+                {
+                    Console.WriteLine("ahh, but too low-- go higher");
+
+                }
+
+                else if (userInput > FavNumber)
+                {
+                    Console.WriteLine("whoops, too high. Come down some");
+                }
+
+                else (userInput = FavNumber)
+
+                        {
+                    Console.WriteLine("BNGO!");
+                }
+            }
+
+             
+        }
+    }
+}
